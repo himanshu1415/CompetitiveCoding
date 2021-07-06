7bc4aaeca433639083cbe636a778693f2b9f1e84
himanshu dhaka <1415himanshu1415@gmail.com>
2021-07-06 21:51:20 +0530

    Add files via upload
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/

---
+++ Best-time-to-buy-stock-market-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int sp = prices[0], cp = prices[0], profit = 0,sum=0,flag;
+		for(int i=0; i<prices.size(); i++) {
+			if (prices[i] <= cp) {
+				cp = prices[i];
+				sp = prices[i];
+			}
+			else {
+				sp = prices[i];
+                profit = max(profit, sp - cp);
+                flag=1;
+                sum = sum+profit; 
+			}
+            if(flag==1)
+            {
+                cp = prices[i];
+				sp = prices[i];
+                profit=0;
+            }
+            
+		}
+		return sum;        
+    }
+};

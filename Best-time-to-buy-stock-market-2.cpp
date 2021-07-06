727b00336872e2a1eaf654c4b7fe4942d78ed124
himanshu dhaka <1415himanshu1415@gmail.com>
2021-07-06 21:52:45 +0530

    Update Best-time-to-buy-stock-market-2.cpp

--- Best-time-to-buy-stock-market-2.cpp
+++ Best-time-to-buy-stock-market-2.cpp
@@ -24,3 +24,19 @@ public:
 		return sum;        
     }
 };
+
+
+//  Alternative solution
+/*
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+       int maxprofit = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;        
+    }
+};
+*/
